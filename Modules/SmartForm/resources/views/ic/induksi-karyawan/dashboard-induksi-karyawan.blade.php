f9f272f4251d989c36f696a8525e04d76be03718
Abiyoga Hendra <abiyogakhusus@gmail.com>
2024-09-03 13:24:05 +0700

    perubahan mekanisme penginputan untuk induksi karyawan

--- Modules/SmartForm/resources/views/ic/induksi-karyawan/dashboard-induksi-karyawan.blade.php
+++ Modules/SmartForm/resources/views/ic/induksi-karyawan/dashboard-induksi-karyawan.blade.php
@@ -57,9 +57,13 @@
 
                 </div>
                 <div class="card-body px-0 pb-2">
-                    <div class="d-flex align-items-center">
-                        <a href="{{ route('bss-form-ic-induksi-karyawan') }}"><button class="btn btn-primary ms-auto uploadBtn">
-                                Mulai Induksi</button></a>
+                    <div class="d-flex align-items-center" style="margin:10px">
+                        <div class="col-md-2">
+                            <a href="{{ route('bss-form-ic-induksi-karyawan') }}"><button
+                                    class="btn btn-primary ms-auto uploadBtn">
+                                    Mulai Induksi</button></a>
+                        </div>
+
                     </div>
                     <div class="table-responsive p-0">
                         <table id="dataListFormICInduksiKaryawan" data-toggle="table"
@@ -70,13 +74,12 @@
                             data-unique-id="id">
                             <thead>
                                 <tr>
-                                    <th data-field="NIK" data-align="center" data-halign="center">NIK</th>
-                                    <th data-field="Nama" data-align="center" data-halign="center">Nama</th>
-                                    <th data-field="Jabatan" data-align="center" data-halign="center">Jabatan</th>
-                                    <th data-field="Instansi" data-align="center" data-halign="center">Instansi</th>
-                                    <th data-field="Group" data-align="center" data-halign="center">Jenis</th>
-                                    <th data-field="created_at" data-align="center" data-formatter="dataTableDateFormater"
-                                        data-halign="center">Tanggal</th>
+                                    <th data-field="code" data-align="center" data-halign="center">Code</th>
+                                    <th data-field="link" data-align="center" data-halign="center">link</th>
+                                    <th data-field="expired" data-align="center" data-formatter="dataTableDateFormater"
+                                        data-halign="center">Expired</th>
+                                    <th data-field="jml_karyawan" data-align="center" data-halign="center">Karyawan</th>
+                                    <th data-field="pertanyaan" data-align="center" data-halign="center">Jenis</th>
                                     <th data-halign="center" data-align="center"
                                         data-formatter="dataListFormICInduksiKaryawanActionFormater">Action
                                     </th>
@@ -89,6 +92,7 @@
         </div>
     </div>
 @endsection
+
 
 @section('custom-js')
     <script src="https://unpkg.com/gijgo@1.9.14/js/gijgo.min.js" type="text/javascript"></script>
@@ -141,7 +145,8 @@
         function RedirectToDetail(obj) {
             let indexDt = $(obj).closest('tr').data('index');
             let d = $('#dataListFormICInduksiKaryawan').bootstrapTable('getData')[indexDt];
-            window.location.href = "/bss-form/induksi-karyawan/form-edit-view-IC-form-induksi/" + d.NIK + "=" + d.created_at;
+            window.location.href = "/bss-form/induksi-karyawan/form-edit-view-IC-form-induksi/" + d.code + "=" + d
+                .created_at;
         }
 
         function SubmittAllDataModal() {
@@ -236,12 +241,6 @@
             return data;
         }
 
-        function redirectToAddStepPica(obj) {
-            var indexDt = $(obj).closest('tr').data('index');
-            window.location.href = "/smart-pica/create-step/" + $('#dataListFormICInduksiKaryawan').bootstrapTable('getData')[
-                    indexDt]
-                .nodocpica;
-        }
 
         function dataListFormICInduksiKaryawanParamsGenerate(params) {
 
