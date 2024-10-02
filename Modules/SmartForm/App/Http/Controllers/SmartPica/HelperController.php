5ab7918554a206e6569e1d3dc16d20a95f27d349
Abiyoga Hendra <abiyogakhusus@gmail.com>
2024-10-02 11:27:58 +0700

    penambahan flow ketika closing task kena reject

--- Modules/SmartForm/App/Http/Controllers/SmartPica/HelperController.php
+++ Modules/SmartForm/App/Http/Controllers/SmartPica/HelperController.php
@@ -274,11 +274,13 @@ class HelperController extends Controller
     function HelperDataTableStepSolutionPica(Request $table)
     {
         $nik = session("user_id");
-        $query = "  WITH dataProgress AS (
+        $query = " WITH dataProgress AS (
                         SELECT
                             id_solution,
                             nodocpica,
                             TRY_CAST(progress AS INT) AS progress,
+                            status_reject,
+                            keterangan_reject,
                             ROW_NUMBER() OVER (PARTITION BY id_solution, nodocpica ORDER BY TRY_CAST(progress AS INT) DESC) AS rn
                         FROM
                             history_progress_solution
@@ -288,13 +290,17 @@ class HelperController extends Controller
                         step_pica.id_master,
                         ISNULL(dp.progress, 0) AS progress,
                         UPPER(CASE
+                            WHEN ISNULL(step_pica.acceptance, 0) = 2 THEN 'REJECT BY PIC'
+                            WHEN ISNULL(step_pica.acceptance, 0) = 9 AND ISNULL(step_pica.status_approve, 0) = 0 AND ISNULL(step_pica.status_reject, 0) = 0  THEN 'NEED APPROVE'
                             WHEN ISNULL(dp.progress, 0) = 0 THEN 'NOT YET'
                             WHEN ISNULL(dp.progress, 0) > 0 AND ISNULL(dp.progress, 0) < ISNULL(mp.target_master, 0) THEN 'ON PROGRESS'
-                            WHEN ISNULL(dp.progress, 0) = ISNULL(mp.target_master, 0) THEN 'CLOSE'
+                            WHEN ISNULL(step_pica.acceptance, 0) = 9 AND ISNULL(step_pica.status_approve, 0) = 1 THEN 'CLOSE'
+                            WHEN ISNULL(dp.status_reject,0) = 1 THEN 'REVISION'
                             ELSE 'NOT YET'
                         END) AS status,
                         step_pica.nik_master,
                         step_pica.id,
+                        dp.keterangan_reject,
                         CASE
                             WHEN step_pica.action = 'ca' THEN 'Corrective'
                             WHEN step_pica.action = 'pa' THEN 'Preventive'
@@ -361,6 +367,7 @@ class HelperController extends Controller
                     ,[ccp]
                     ,[progress]
                     ,[created_at]
+                    ,status_reject
                 FROM [history_progress_solution] where id_solution = '" . $table->search['IDSOLUTION'] . "' and nodocpica = '" . $table->search['NODOCPICA'] . "' ";
         $countDataUser = DB::select("select count(*) jumlah FROM history_progress_solution where id_solution = '" . $table->search['IDSOLUTION'] . "' and nodocpica = '" . $table->search['NODOCPICA'] . "' ");
         $newQuery = $this->GetQueryDataTableHistoryTable($query, req: $table);
