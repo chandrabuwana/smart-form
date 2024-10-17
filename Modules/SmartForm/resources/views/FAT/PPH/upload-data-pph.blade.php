01e3d6cc428312899cbf68e9404ba0b8a02b603a
Abiyoga Hendra <abiyogakhusus@gmail.com>
2024-10-17 11:44:25 +0700

    Perubahan untuk data Potongan PPH

---
+++ Modules/SmartForm/resources/views/FAT/PPH/upload-data-pph.blade.php
@@ -0,0 +1,409 @@
+@extends('master.master_page')
+
+@section('custom-css')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.6/dist/bootstrap-table.min.css">
+    <style>
+        .display-block {
+            display: contents !important;
+        }
+
+        .select2-dropdown {
+            overflow: scroll;
+            height: 300px;
+        }
+
+        .custom-width-1 {
+            width: 90%;
+            /* Example width, adjust as needed */
+        }
+
+
+        .close-button-why {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 30px;
+            height: 30px;
+            border: none;
+            border-radius: 50%;
+            background-color: black;
+            color: white;
+            font-size: 16px;
+            font-weight: bold;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            transform: translate(50%, -50%);
+        }
+
+        .close-button-why:hover {
+            background-color: darkred;
+        }
+
+        .select2-container--bootstrap-5 .select2-selection--single {
+            height: calc(1.5em + .75rem + 2px);
+            /* Menyesuaikan dengan form-control di Bootstrap 5 */
+        }
+
+        /* Menyesuaikan tinggi baris teks dalam pilihan */
+        .select2-container--bootstrap-5 .select2-selection__rendered {
+            line-height: calc(1.5em + .75rem + 2px);
+        }
+
+        .scrollable-div {
+            width: 100%;
+            height: 400px;
+            overflow: auto;
+            position: relative;
+        }
+
+        fieldset {
+            border: 2px solid #ddd;
+            /* Border for the fieldset */
+            padding: 1.5em;
+            /* Padding inside the fieldset */
+            margin-bottom: 1.5em;
+            /* Margin below the fieldset */
+            position: relative;
+            /* Position relative to handle absolute positioned legend */
+        }
+
+        legend {
+            font-size: 1.25em;
+            /* Font size for the legend */
+            font-weight: bold;
+            /* Make the legend text bold */
+            padding: 0 10px;
+            /* Padding to give some space on left and right */
+            background-color: white;
+            /* Background color to match the page's background */
+            position: absolute;
+            /* Position the legend absolutely */
+            top: -1em;
+            /* Move it up above the border */
+            left: 10px;
+            /* Adjust left position */
+        }
+
+        .zoomable-content {
+            transform-origin: 0 0;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card my-4">
+                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                    <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+                        <h6 class="text-white text-capitalize ps-3">Form Induksi Karyawan</h6>
+                    </div>
+                </div>
+                <div class="card-body my-1">
+                    <div class="row gx-4">
+                        <div class="col-auto my-auto ms-3">
+                            <div class="h-100">
+                                <p class="mb-0 fw-bold text-sm">
+                                    Pelapor : {{ session('username') }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="card-body">
+                        <div class="row" id="tabel_tambah">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <div class="input-group input-group-static my-3">
+                                        <label for="pc_thn" class="ms-0">Tahun </label>
+                                        <select class="form-control" name="pc_thn" id="pc_thn" required>
+                                            <option value="">-- Pilih Tahun --</option>
+                                            <?php
+                                            $y = date('Y');
+                                            for ($i = $y - 2; $i < $y + 4; $i++) {
+                                                echo "<option value='$i'>$i</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="input-group input-group-static my-3">
+                                        <label for="pc_bln" class="ms-0">Bulan </label>
+                                        <select class="form-control" name="pc_bln" id="pc_bln" required>
+                                            <option value="">-- Pilih Bulan --</option>
+                                            <?php
+                                            for ($month = 1; $month <= 12; $month++) {
+                                                $monthName = date('F', mktime(0, 0, 0, $month, 1));
+                                                echo "<option value='$month'>$monthName</option>";
+                                            }
+                                            ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="input-group input-group-static my-3">
+                                        <label for="pc_site" class="ms-0">Site </label>
+                                        <select class="form-control site" name="pc_site" id="pc_site">
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="input-group input-group-static my-4">
+                                        <label for="pc_zip" class="ms-0">Upload Data ZIP </label>
+                                        <input type="file" class="form-control" name="pc_zip" id="pc_zip" />
+                                        <small id="fileError" style="color:red; display:none;">Please upload a valid ZIP
+                                            file.</small>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <button id="buttonGenerateLinkButton" class="btn btn-primary ms-auto uploadBtn"
+                                            onclick="ExtractZip()">
+                                            Extract ZIP</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="horizontal dark my-sm-4">
+                        <div class="row" id="tabel_list_karyawan">
+                            <div class="row" style="padding-left: 0px !important;">
+                                <div class="col" style="padding-left: 0px !important;">
+                                    <input type="hidden" name="nodocpph" id="nodocpph">
+                                    <fieldset class="color-fieldset form-horizontal">
+                                        <legend class="color-legend">
+                                            <span>Daftar dokument yang sudah terupload</span>
+                                        </legend>
+                                        <div class="form-horizontal">
+                                            <table id="tableListOfDocumentUploaded" data-toggle="table"
+                                                data-ajax="tableListOfDocumentUploadedGenerateData"
+                                                data-query-params="tableListOfDocumentUploadedParamsGenerate"
+                                                data-side-pagination="server" data-page-list="[10, 25, 50, 100, all]"
+                                                data-sortable="true" data-content-type="application/json"
+                                                data-data-type="json" data-pagination="true" data-unique-id="id">
+                                                <thead>
+                                                    <tr>
+                                                            <th data-field="nama_file" data-halign="center" data-sortable="true">
+                                                            Nama Document</th>
+                                                        <th data-field="npwp" data-halign="center"
+                                                            data-sortable="true">
+                                                            NPWP</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody></tbody>
+                                            </table>
+                                        </div>
+                                    </fieldset>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row justify-content-between">
+                            <div class="col-md-3">
+                                <button class="btn btn-primary ms-auto back-button-by-history">
+                                    <i class="fas fa-back"></i>
+                                    Back Page</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@section('custom-js')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.22.6/dist/bootstrap-table.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://unpkg.com/gijgo@1.9.14/js/gijgo.min.js" type="text/javascript"></script>
+    <link href="https://unpkg.com/gijgo@1.9.14/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript">
+        var MateriTambahanInputData_Obj_datas = [];
+
+        function ExtractZip() {
+            let isValid = true;
+
+            if ($('#pc_thn').val() === '') {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Silakan pilih tahun!',
+                });
+                isValid = false;
+            } else if ($('#pc_bln').val() === '') {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Silakan pilih bulan!',
+                });
+                isValid = false;
+            } else if ($('#pc_site').val() === '') {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Silakan pilih site!',
+                });
+                isValid = false;
+            }
+            let fileInput = $('#pc_zip')[0].files[0]; // Get the file from input
+
+            if (!fileInput) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Silakan pilih file untuk diunggah!',
+                });
+                isValid = false;
+            } else if (fileInput.type !== 'application/zip' && fileInput.name.split('.').pop() !== 'zip') {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'File harus berformat .zip!',
+                });
+                isValid = false;
+            }
+
+            if (!isValid) {
+                return false;
+            }
+            let dataKirim = new FormData();
+
+            dataKirim.append('site', $('#pc_site').val());
+            dataKirim.append('tahun', $('#pc_thn').val());
+            dataKirim.append('bulan', $('#pc_bln').val());
+
+            dataKirim.append('zip', fileInput);
+
+            $.ajax({
+                type: 'post',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                url: "process-data-upload",
+                data: dataKirim,
+                processData: false, // Important for file upload
+                contentType: false, // Important for file upload
+                dataType: 'json',
+                success: function(response) {
+                    if (response.code == 200) {
+                        Swal.fire({
+                            icon: "success",
+                            title: "Done",
+                        });
+                        $('#pc_zip').val('');
+                        $('#pc_bln').val('');
+                        $('#pc_thn').val('');
+                        $('#pc_site').val('');
+                        $('#nodocpph').val(response.codepph);
+                        $('#tableListOfDocumentUploaded').bootstrapTable('refresh');
+
+                    } else {
+                        Swal.fire({
+                            icon: "error",
+                            title: "Oops...",
+                            text: response.data,
+                        });
+                    }
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'thrownError',
+                        html: thrownError,
+                        confirmButtonText: 'OK'
+                    });
+                }
+            })
+        }
+    </script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#pc_site').select2({
+                theme: 'bootstrap-5', // Menggunakan tema Bootstrap 5
+                dropdownParent: $('#pc_site').closest('.input-group'),
+                placeholder: '--- Cari Site ---',
+                ajax: {
+                    url: "/helper/site",
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    type: "post",
+                    delay: 250,
+                    dataType: 'json',
+                    data: function(params) {
+                        return {
+                            _token: "{{ csrf_token() }}",
+                            query: params.term, // search term
+                        };
+                    },
+                    processResults: function(response) {
+                        return {
+                            results: response.data
+                        };
+                    },
+                    cache: true
+                }
+            });
+
+            $('#pc_site').on('select2:open', function(e) {
+                const evt = "scroll.select2";
+                $(e.target).parents().off(evt);
+                $(window).off(evt);
+            });
+
+            $('#pc_zip').val('');
+            $('#pc_bln').val('');
+            $('#pc_thn').val('');
+            $('#pc_site').val('');
+
+            $('#pc_zip').on('change', function() {
+                var fileInput = $('#pc_zip')[0];
+                var fileError = $('#fileError');
+                var file = fileInput.files[0];
+
+                if (file) {
+                    var fileName = file.name;
+                    var fileSize = file.size / 1024 / 1024; // file size in MB
+                    var fileExtension = fileName.split('.').pop().toLowerCase();
+
+                    if (fileExtension !== 'zip') { // Adjust max file size if needed (e.g., 5MB)
+                        fileError.show();
+                        $('#pc_zip').val(''); // Clear the input
+                    } else {
+                        fileError.hide();
+                    }
+                }
+            });
+
+        });
+
+        function tableListOfDocumentUploadedParamsGenerate(params) {
+
+            params.search = {
+                'FILTERNODOC': $('#nodocpph').val()
+            };
+
+            if (params.sort == undefined) {
+                return {
+                    limit: params.limit,
+                    offset: params.offset,
+                    search: params.search
+                }
+            }
+            return params;
+        }
+
+        function tableListOfDocumentUploadedGenerateData(params) {
+            var url = 'lst-fat-doc-list-uploaded'
+            $.get(url + '?' + $.param(params.data)).then(function(res) {
+                params.success(res)
+            })
+        }
+    </script>
+@endsection
--- Modules/SmartForm/routes/web.php
+++ Modules/SmartForm/routes/web.php
@@ -30,6 +30,8 @@ use Modules\SmartForm\App\Http\Controllers\SmartPica\TransactionPicaController;
 use Modules\SmartForm\App\Http\Controllers\TeamManagement\RoleManagementController;
 use Modules\SmartForm\App\Http\Controllers\TeamManagement\UserManagementController;
 use Modules\SmartForm\App\Http\Controllers\UnderCarriage\UnderCarriageInspectionController;
+use Modules\SmartForm\App\Http\Controllers\FAT\PPH\PPHDashboardController;
+use Modules\SmartForm\App\Http\Controllers\FAT\PPH\HelperPPHController;
 
 /*
 |--------------------------------------------------------------------------
@@ -91,6 +93,19 @@ Route::group(['middleware' => ['check.auth', FetchMenu::class, PermissionMenu::c
             Route::get('/download-pdf/{id}', [ICFM05InduksiKaryawanController::class, 'downloadPDF']);
         });
 
+        Route::prefix('fat')->group(function () {
+            Route::prefix('pph')->group(function () {
+                Route::get('/dashboard', [PPHDashboardController::class, 'DashboardIndex'])->name("bss-dahboard-fat-pph-dashboard");
+                Route::get('/add-data-upload', [PPHDashboardController::class, 'AddDatadIndex'])->name("bss-dahboard-fat-pph-add");
+                Route::get('/lst-fat-doc-list-uploaded', [HelperPPHController::class, 'helperDataListHasilUploadDocument']);
+                Route::get('/lst-fat-doc-list-master', [HelperPPHController::class, 'helperDataListMasterUploadDocumentPPH']);
+                Route::post('/process-data-upload', [PPHDashboardController::class, 'ProcessZIPUpload']);
+                Route::post('/hapus-document-potongan', [HelperPPHController::class, 'HapusDocumentPotonganPPH']);
+                Route::post('/update-document-potongan', [HelperPPHController::class, 'UpdateDocumentPotonganPPH']);
+                Route::get('/view-detail-master-potongan-pph/{id}', [PPHDashboardController::class, 'indexViewDataDetailMasterPPh']);
+            });
+        });
+
         Route::prefix('she-019B')->group(function () {
             Route::get('/dashboard', [DashboardSHEFRM19BController::class, 'DashboardIndex'])->name("bss-form-she-019B");
             Route::get('/bss-form-she-019B-add-frm', [DashboardSHEFRM19BController::class, 'AddForm'])->name("add-bss-form-she-019B");
@@ -111,7 +126,7 @@ Route::group(['middleware' => ['check.auth', FetchMenu::class, PermissionMenu::c
             // Route::post('/generate-detail-pemesanan-catering', [SmartCateringController::class, 'GenerateDetailPemesanan'])->name('generate-detail-pemesanan-catering');
         });
 
-        Route::prefix('catering')->group( function() {
+        Route::prefix('catering')->group(function () {
             Route::get('/pemesanan', [SmartCateringController::class, 'AddPemesanan'])->name('add-pemesanan-catering');
             Route::get('/dashboard-pemesanan', [SmartCateringController::class, 'DashboardPemesanan'])->name('dashboard-pemesanan-catering');
             Route::get('/detail-pemesanan', [SmartCateringController::class, 'DetailPemesanan'])->name('detail-pemesanan-catering');
@@ -119,7 +134,7 @@ Route::group(['middleware' => ['check.auth', FetchMenu::class, PermissionMenu::c
             Route::post('/generate-detail', [SmartCateringController::class, 'GenerateDetailPemesanan'])->name('generate-detail-pemesanan-catering');
             Route::post('/order', [SmartCateringController::class, 'SubmitPesanMakan'])->name('submit-makan');
 
-            Route::prefix('mess')->group( function() {
+            Route::prefix('mess')->group(function () {
                 Route::post('/add-mess', [MessController::class, 'AddMess'])->name('add-mess');
                 Route::post('/add-kamar', [MessController::class, 'AddKamar'])->name('add-kamar');
                 Route::post('/add-penghuni', [MessController::class, 'AddPenghuniMess'])->name('add-penghuni');
@@ -139,7 +154,7 @@ Route::group(['middleware' => ['check.auth', FetchMenu::class, PermissionMenu::c
 
             });
 
-            Route::prefix('vendor')->group( function() {
+            Route::prefix('vendor')->group(function () {
                 Route::get('/helper-vendor', [VendorController::class, 'HelperVendor'])->name('helper-vendor');
                 Route::get('/helper-lokasi', [VendorController::class, 'HelperLokasi'])->name('helper-lokasi');
                 Route::get('/dashboard-vendor', [VendorController::class, 'DashboardVendor'])->name('dashboard-vendor');
@@ -161,19 +176,19 @@ Route::group(['middleware' => ['check.auth', FetchMenu::class, PermissionMenu::c
     Route::post('/add-new-menu', [AdminController::class, 'AddNewMenu'])->name('add-new-menu');
     Route::put('/edit-menu', [AdminController::class, 'EditMenu'])->name('edit-menu');
     Route::delete('/delete-menu', [AdminController::class, 'DeleteMenu'])->name('delete-menu');
-    
+
     Route::get('/dashboard-level-user', [MappingValidationController::class, 'IndexLevelUser']);
     Route::get('/list-level-user', [MappingValidationController::class, 'GetListLevelUser']);
     Route::post('/add-level-user', [MappingValidationController::class, 'AddLevelUser']);
     Route::put('/edit-level-user', [MappingValidationController::class, 'EditLevelUser']);
     Route::delete('/delete-level-user', [MappingValidationController::class, 'DeleteLevelUser']);
-    
+
     Route::get('/dashboard-level-mapping', [MappingValidationController::class, 'IndexlevelMapping']);
     Route::get('/list-level-mapping', [MappingValidationController::class, 'GetListLevelMapping']);
     Route::post('/add-level-mapping', [MappingValidationController::class, 'AddLevelMapping']);
     Route::put('/edit-level-mapping', [MappingValidationController::class, 'EditLevelMapping']);
     Route::delete('/delete-level-mapping', [MappingValidationController::class, 'DeleteLevelMapping']);
-    
+
     Route::get('/dashboard-section-department', [SectionDepartmentController::class, 'IndexSectionDepartment']);
     Route::get('/list-section-department', [SectionDepartmentController::class, 'GetListSectionDepartment']);
     Route::post('/add-section-department', [SectionDepartmentController::class, 'AddSectionDept']);
@@ -237,10 +252,10 @@ Route::group(['middleware' => ['check.auth', FetchMenu::class, PermissionMenu::c
         Route::post('/deleted-progress-history-transaction', [TransactionPicaController::class, 'deleteTransactionProgressStepSolutionPica']);
         Route::post('/change-acceptance', [TransactionPicaController::class, 'changeAcceptanceStepSolutionPica']);
         Route::post('/approve-task-closing', [TransactionPicaController::class, 'ApproveClosingTask']);
-        
+
     });
 
-    Route::prefix('skl')->group( function() {
+    Route::prefix('skl')->group(function () {
         Route::get('/dashboard', [DashboardSKLController::class, 'dashboard'])->name('bss-skl.dashboard');
         Route::get('/dashboard/get-data', [DashboardSKLController::class, 'getDashboardData'])->name('bss-skl.dashboard-get-data');
         Route::get('/form', [SKLFormController::class, 'create'])->name('bss-skl.create');
@@ -252,6 +267,7 @@ Route::group(['middleware' => ['check.auth', FetchMenu::class, PermissionMenu::c
         Route::post('/approval', [DashboardSKLController::class, 'storeApproval'])->name('bss-skl.store-approval');
     });
 
+
     Route::prefix('approval')->group(function () {
         Route::post('/form', [ApprovalFormController::class, 'approveForm'])->name('bss-approval-form');
     });
@@ -261,6 +277,7 @@ Route::group(['middleware' => ['check.auth', FetchMenu::class, PermissionMenu::c
     });
 
     Route::get('/landing-page-dashboard', [DashboardController::class, 'DashboardIndex']);
+
 });
 
 Route::get('/bss-form/induksi-karyawan/listing-karyawan/{data}', [ICFM05InduksiKaryawanController::class, 'indexFormAddKaryawanListing']);
