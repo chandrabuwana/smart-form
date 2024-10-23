f354e7a796653cbf24c36813f704143f2f5a95e3
Muhammad Randika Rosyid <randika.rosyid2@gmail.com>
2024-10-23 14:34:39 +0700

    fix download excel skl

--- Modules/SmartForm/App/Http/Controllers/SKL/DashboardSKLController.php
+++ Modules/SmartForm/App/Http/Controllers/SKL/DashboardSKLController.php
@@ -224,8 +224,6 @@ class DashboardSKLController extends Controller
         $site    = $request->query('site', '');
         $status   = $request->query('status', '');
         $tanggal   = $request->query('tanggal', '');
-        $offset  = $request->query('offset', 0);
-        $limit   = $request->query('limit', 10);
 
         $headers = [
             'No. Form',
@@ -235,14 +233,14 @@ class DashboardSKLController extends Controller
             'Nama',
             'NIK',
             'Jabatan',
-            // 'Kategori Pekerjaan',
-            // 'Detail Pekerjaan',
-            // 'Jam Mulai',
-            // 'Jam Selesai',
-            // 'Jam Konversi',
+            'Kategori Pekerjaan',
+            'Detail Pekerjaan',
+            'Jam Mulai',
+            'Jam Selesai',
+            'Total Konversi',
         ];
 
-        foreach(range('A', 'F') as $key => $column) {
+        foreach(range('A', 'K') as $key => $column) {
             $worksheet->getColumnDimension($column)->setAutoSize(true);
 
             $cellStyle = $worksheet->getStyle($column . '1');
@@ -252,12 +250,28 @@ class DashboardSKLController extends Controller
             $worksheet->getCell($column . '1')->setValue( $headers[$key] );
         }
 
-        $qExport = DB::table(self::T_FORM_KARYAWAN)->select(self::T_FORM_MST . '.NoForm', self::T_DEPARTEMENT . '.Nama AS NamaDP', self::T_SITE . '.Nama AS NamaSite', self::T_FORM_MST . '.Shift', self::T_KARYAWAN . '.Nama AS NamaKaryawan', self::T_KARYAWAN . '.NIK', self::T_JABATAN . '.Nama AS NamaJabatan')
+        $qExport = DB::table(self::T_FORM_KARYAWAN)->select(
+                self::T_FORM_MST . '.NoForm',
+                self::T_DEPARTEMENT . '.Nama AS NamaDP',
+                self::T_SITE . '.Nama AS NamaSite',
+                self::T_FORM_MST . '.Shift',
+                self::T_KARYAWAN . '.Nama AS NamaKaryawan',
+                self::T_KARYAWAN . '.NIK',
+                self::T_JABATAN . '.Nama AS NamaJabatan',
+                self::T_MST_PEKERJAAN . '.Nama AS NamaPekerjaan',
+                self::T_FORM_PEKERJAAN . '.Detail AS DetailPekerjaan',
+                self::V_JAM_KONVERSI . '.JamMulai',
+                self::V_JAM_KONVERSI . '.fix_absen_out AS JamSelesai',
+                self::V_JAM_KONVERSI . '.total_konversi_jam AS TotalKonversi'
+            )
             ->join(self::T_FORM_MST, self::T_FORM_MST . '.NoForm', '=', self::T_FORM_KARYAWAN . '.NoForm')
+            ->join(self::V_JAM_KONVERSI, self::V_JAM_KONVERSI . '.NoForm', '=', self::T_FORM_MST . '.NoForm')
             ->join(self::T_SITE, self::T_SITE . '.KodeST', '=', self::T_FORM_MST . '.KodeST')
             ->join(self::T_DEPARTEMENT, self::T_DEPARTEMENT . '.KodeDP', '=', self::T_FORM_MST . '.KodeDepartement')
             ->join(self::T_KARYAWAN, self::T_KARYAWAN . '.NIK', '=', self::T_FORM_KARYAWAN . '.NIK')
             ->join(self::T_JABATAN, self::T_JABATAN . '.KodeJB', '=', self::T_KARYAWAN . '.KodeJB')
+            ->join(self::T_FORM_PEKERJAAN, self::T_FORM_PEKERJAAN . '.NoForm', '=', self::T_FORM_MST . '.NoForm')
+            ->join(self::T_MST_PEKERJAAN, self::T_MST_PEKERJAAN . '.ID', '=', self::T_FORM_PEKERJAAN . '.IDPekerjaan')
             ->where('created_by', $userid);
 
         if(!empty($departement)) {
@@ -287,13 +301,18 @@ class DashboardSKLController extends Controller
             $worksheet->getCell('E' . $i)->setValue($item->NamaKaryawan);
             $worksheet->getCell('F' . $i)->setValue($item->NIK);
             $worksheet->getCell('F' . $i)->setValue($item->NamaJabatan);
+            $worksheet->getCell('G' . $i)->setValue($item->NamaPekerjaan);
+            $worksheet->getCell('H' . $i)->setValue($item->DetailPekerjaan);
+            $worksheet->getCell('I' . $i)->setValue($item->JamMulai);
+            $worksheet->getCell('J' . $i)->setValue($item->JamSelesai);
+            $worksheet->getCell('K' . $i)->setValue($item->TotalKonversi);
 
             $i++;
         }
 
         $writer = new Xlsx($spreadsheet);
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment; filename="test.xlsx"');
+        header('Content-Disposition: attachment; filename="Report SKL.xlsx"');
         $writer->save('php://output');
     }
 }
