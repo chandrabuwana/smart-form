27a441bfd92c93600340f1c1d0dc3714027cd862
jimmy feriawan <feriawanjimmy@gmail.com>
2024-10-31 17:23:01 +0700

    fix : absen dari tabsensi

--- Modules/SmartForm/App/Http/Controllers/GS/SmartCateringController.php
+++ Modules/SmartForm/App/Http/Controllers/GS/SmartCateringController.php
@@ -132,22 +132,27 @@ class SmartCateringController extends Controller {
 
                 } else {
                     // tabel absensi
-                    // $data_karyawan_absensi = DB::connection(self::DB_CONN_NAME)->table($this->DB_LINK[$reqSite] . self::TABLE_ABSENSI_HRD . ' as ta')
-                    //     ->select('ta.NIK', 'ta.Tanggal', 'ta.Masuk')
-                    //     ->where('lmasuk', $reqSite)
-                    //     ->whereDate('ta.Tanggal', Carbon::createFromFormat('Y-m-d', $reqTanggalPemesanan)->startOfDay()->format('Y-m-d H:i:s.u'))
-                    //     ->whereBetween('ta.Masuk', [$jam_absensi[$selectedShift]['start'], $jam_absensi[$selectedShift]['end']])
-                    //     ;
+                    $data_karyawan_absensi = DB::connection(self::DB_CONN_NAME)->table($this->DB_LINK[$reqSite] . self::TABLE_ABSENSI_HRD . ' as ta')
+                        ->select('ta.NIK', 'ta.Tanggal', 'ta.Masuk')
+                        ->where('lmasuk', $reqSite)
+                        ->where('Shift', 'DS')
+                        ->whereDate('ta.Tanggal', Carbon::createFromFormat('Y-m-d', $reqTanggalPemesanan)->startOfDay()->format('Y-m-d H:i:s.u'))
+                        ->whereBetween('ta.Masuk', [$jam_absensi[$selectedShift]['start'], $jam_absensi[$selectedShift]['end']])
+                        ;
+                    
                     // table fingerlog
-                    $data_karyawan_absensi = DB::connection(self::DB_CONN_NAME)->table($this->DB_LINK[$reqSite] . self::TABLE_FINGERLOG_HRD . ' as tf')
-                        ->select('tf.IP', 'tf.NIK', 'tf.Tanggal', 'tf.Jam as Masuk', 'tk.Nama')
-                        ->leftJoin(self::TABLE_KARYAWAN_HRD . ' as tk', 'tk.Nik', '=', 'tf.Nik')
-                        ->where('tf.Status', 'IN')
-                        ->whereDate('tf.Tanggal', Carbon::createFromFormat('Y-m-d', $reqTanggalPemesanan)->startOfDay()->format('Y-m-d H:i:s.u'))
-                        ->whereBetween('tf.Jam', [$jam_absensi[$selectedShift]['start'], $jam_absensi[$selectedShift]['end']]);
+                    // $data_karyawan_absensi = DB::connection(self::DB_CONN_NAME)->table($this->DB_LINK[$reqSite] . self::TABLE_FINGERLOG_HRD . ' as tf')
+                    //     ->select('tf.IP', 'tf.NIK', 'tf.Tanggal', 'tf.Jam as Masuk', 'tk.Nama')
+                    //     ->leftJoin(self::TABLE_KARYAWAN_HRD . ' as tk', 'tk.Nik', '=', 'tf.Nik')
+                    //     ->where('tf.Status', 'IN')
+                    //     ->whereDate('tf.Tanggal', Carbon::createFromFormat('Y-m-d', $reqTanggalPemesanan)->startOfDay()->format('Y-m-d H:i:s.u'))
+                    //     ->whereBetween('tf.Jam', [$jam_absensi[$selectedShift]['start'], $jam_absensi[$selectedShift]['end']]);
                     Log::debug('SQL absensi karyawan : '. $data_karyawan_absensi->toRawSql());
-                    $data_karyawan_absensi = collect($data_karyawan_absensi->get()->toArray());
-                    $data_karyawan_absensi = $data_karyawan_absensi->unique('NIK')->values()->all();
+                    // tabel absensi
+                    $data_karyawan_absensi = $data_karyawan_absensi->get()->toArray();
+                    // tabel fingerlog
+                    // $data_karyawan_absensi = collect($data_karyawan_absensi->get()->toArray());
+                    // $data_karyawan_absensi = $data_karyawan_absensi->unique('NIK')->values()->all();
                     Log::debug("Data absensi ". $reqSite . " : " .count($data_karyawan_absensi));
                 }
 
