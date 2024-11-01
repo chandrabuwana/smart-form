ff392240918205cfdc72c3fb84c71f2bdce65d39
Abiyoga Hendra <abiyogakhusus@gmail.com>
2024-11-01 08:52:08 +0700

    perbaikan untuk smart pica perihal week

--- Modules/SmartForm/resources/views/smartpica/add-step-pica.blade.php
+++ Modules/SmartForm/resources/views/smartpica/add-step-pica.blade.php
@@ -4,8 +4,8 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <style>
         /* .gj-icon {
-                                                                                                                                                                                                                                                                                                                                            display: none !important;
-                                                                                                                                                                                                                                                                                                                                        } */
+                                                                                                                                                                                                                                                                                                                                                display: none !important;
+                                                                                                                                                                                                                                                                                                                                            } */
 
         /* Hide the datepicker button */
         .gj-datepicker button {
@@ -146,7 +146,7 @@
                         <label for="staticEmail" class="col-sm-2 col-form-label">Week</label>
                         <div class="col-sm-7">
                             <input type="text" readonly class="form-control-plaintext"
-                                value="Week {{ $dataMaster[0]->week + 1 }}">
+                                value="Week {{ $dataMaster[0]->week }}">
                         </div>
                     </div>
                     <div class="row">
