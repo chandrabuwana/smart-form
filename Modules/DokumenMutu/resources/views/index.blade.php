327e6b1cf205e650694fb98aff2f21dd914a8f9c
Muhammad Randika Rosyid <randika.rosyid2@gmail.com>
2024-12-02 16:17:49 +0700

    setup dokumen mutu

---
+++ Modules/DokumenMutu/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('master.master_page')
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+        </div>
+    </div>
+@endsection
+
+@section('custom-js')
+    <script type="text/javascript">
+        console.log('Hello World!');
+    </script>
+@endsection
