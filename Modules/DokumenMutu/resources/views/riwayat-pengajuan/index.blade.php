d5594fc29cdf0c0fb324257decaba9a794154640
Muhammad Randika Rosyid <randika.rosyid2@gmail.com>
2024-12-27 11:06:39 +0700

    doco : fix err detail penghapusan

--- Modules/DokumenMutu/resources/views/riwayat-pengajuan/index.blade.php
+++ Modules/DokumenMutu/resources/views/riwayat-pengajuan/index.blade.php
@@ -324,6 +324,7 @@
     <script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.29.0/libs/jsPDF/jspdf.umd.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.23.2/dist/extensions/export/bootstrap-table-export.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.min.js"></script>
 
     @if(session('error'))
         <script>
