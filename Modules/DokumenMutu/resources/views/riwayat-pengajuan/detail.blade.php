6fe17e07a89a31d4471dbc9ada819369fa52324e
Muhammad Randika Rosyid <randika.rosyid2@gmail.com>
2025-01-02 16:47:52 +0700

    doco : fix revisi button

--- Modules/DokumenMutu/resources/views/riwayat-pengajuan/detail.blade.php
+++ Modules/DokumenMutu/resources/views/riwayat-pengajuan/detail.blade.php
@@ -182,7 +182,7 @@
                             {{-- <span class="badge bg-gradient-dark ms-1">{{ $lastVersion->no_versi }}</span> --}}
                         </h5>
 
-                        @if($feedbacks->count() > 0 && $doco->nik_pemohon == $userId)
+                        @if($feedbacks->count() > 0 && $doco->nik_pemohon == $userId && in_array($doco->status, ['Belum Validasi', 'Sedang Validasi']))
                             <button type="button" class="btn bg-gradient-primary mb-0"
                                 onclick="showRevisiModal()">
                                 Buat Revisi
