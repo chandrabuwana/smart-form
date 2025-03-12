84c3784996c9063fb6dc913a9c7d798b84051439
moh raihan romadhoni <126458829+raihan-doni16@users.noreply.github.com>
2025-03-12 22:30:24 +0700

    fix accordion sidebar

--- Modules/SmartForm/resources/views/plant/ppm_900d/show-900d.blade.php
+++ Modules/SmartForm/resources/views/plant/ppm_900d/show-900d.blade.php
@@ -636,9 +636,6 @@
 @section('custom-js')
     <script src="https://cdn.jsdelivr.net/npm/axios@1.7.7/dist/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.all.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
 
 
     <script>
