4f59ef6b74c530e86a771ff4a7f9da8cbcf1661e
ipunganwar <ipung.anwar1@gmail.com>
2025-04-09 23:41:27 +0700

    debug: ergonomi

--- Modules/SmartForm/resources/views/she/ergonomi/form.blade.php
+++ Modules/SmartForm/resources/views/she/ergonomi/form.blade.php
@@ -24,7 +24,7 @@
                 <!-- Card Header -->
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                     <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                        <h6 class="text-white text-capitalize ps-3">{{$isShowDetail ? 'Detail' : 'New'}} Ergonomi Survey</h6>
+                        <h6 class="text-white text-capitalize ps-3">{{$isShowDetail ? 'Detail' : 'New'}} Ergonomi Survey 1</h6>
                     </div>
                 </div>
 
