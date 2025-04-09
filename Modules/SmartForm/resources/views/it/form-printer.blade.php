2791cacadd37a74235a81eb43f701c34a5347158
ipunganwar <ipung.anwar1@gmail.com>
2025-04-09 16:12:19 +0700

    debug: testing bug

--- Modules/SmartForm/resources/views/it/form-printer.blade.php
+++ Modules/SmartForm/resources/views/it/form-printer.blade.php
@@ -87,7 +87,7 @@
         <div class="card">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                 <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                    <h6 class="text-white text-capitalize ps-3">{{$isShowDetail ? 'Detail' : ($isEdit ? 'Edit' : 'New')}} Maintenance Printer</h6>
+                    <h6 class="text-white text-capitalize ps-3">{{$isShowDetail ? 'Detail' : ($isEdit ? 'Edit' : 'New')}} Maintenance Printer 1</h6>
                 </div>
             </div>
             
