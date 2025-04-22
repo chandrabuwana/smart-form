0ee8dfcde2d2e92a0ad565d4dfa5312dff571359
jarisiber <93127252+asimetris@users.noreply.github.com>
2025-04-23 00:21:38 +0700

    Update form-registrasi-supplier.blade.php

--- Modules/SmartForm/resources/views/SM/registrasi-supplier/form-registrasi-supplier.blade.php
+++ Modules/SmartForm/resources/views/SM/registrasi-supplier/form-registrasi-supplier.blade.php
@@ -569,11 +569,11 @@
     <script>
         function previewImage(event) {
          var input = event.target;
-         var image = document.getElementById('npwp');
+         var npwp = document.getElementById('npwp');
          if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = function(e) {
-               image.src = e.target.result;
+               npwp.src = e.target.result;
             }
             reader.readAsDataURL(input.files[0]);
          }
