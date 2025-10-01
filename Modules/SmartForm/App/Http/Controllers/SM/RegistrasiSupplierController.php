fb79c0c78cc0897669c3356501a79e632963e64f
ipunganwar <ipung.anwar1@gmail.com>
2025-10-01 16:00:27 +0700

    fix: registrasi supplier

--- Modules/SmartForm/App/Http/Controllers/SM/RegistrasiSupplierController.php
+++ Modules/SmartForm/App/Http/Controllers/SM/RegistrasiSupplierController.php
@@ -192,11 +192,7 @@ class RegistrasiSupplierController extends Controller {
                 'diterima_oleh' => $request->dDiterima,
                 'status' => "3",
                 'created_at' => $today,
-<<<<<<< Updated upstream
                 'is_active' => "1",
-=======
-                'is_active' => 1,
->>>>>>> Stashed changes
                 'disetujui_oleh' => $request->dApproved
 
             ]);
